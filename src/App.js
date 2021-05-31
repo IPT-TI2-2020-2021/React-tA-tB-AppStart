a28089d50d76c16535629a7351de8f83257bdf4a
Jose Casimiro Nunes Pereira <casimiro@ipt.pt>
2021-05-31 16:09:58 +0100

    app comentada

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
+// cria uma variável e atribui-lhe um valor
 import logo from './logo.svg';
+// associa o ficheiro CSS ao componente
 import './App.css';
 
+
+// cria o 'componente' App
+// neste caso é criada uma FUNÇÃO
 function App() {
+  // todo o componente tem obrigatoriamente de 'devolver' alguma coisa
   return (
-    <div className="App">
+    <div className="App"> {/* e apenas consegue devolver um ÚNICO objeto */}
+    {/* utiliza-se o nome 'className' em vez de 'class' 
+        porque no JSX a palavra 'class' é reservada.
+        Na conversão para HTML, a palavra 'className' é convertida em 'class' */}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {/* a forma de se aceder a variáveis no React
+            é identificar essa variável entre chavetas {logo} */}
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edite este ficheiro <code>src/App.js</code> e guarde-o, 
+          que é efetuado automaticamente o 'recargar'.
         </p>
         <a
           className="App-link"
@@ -15,11 +27,13 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Aprender React
         </a>
       </header>
     </div>
   );
-}
+} // fim da 'componente' App
 
+
+// um componente só é acessível se for 'exportado'
 export default App;
